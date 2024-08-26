2d1bdfee917b3eab4545a429bf662fc097c688ed
Marinajwb <byun8688@gmail.com>
2024-08-26 17:03:43 +0900

    findALl() 함수 사용 , 링크 , 리다이렉트 상세페이지 0826

--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-
+@NoArgsConstructor
 @ToString
 @AllArgsConstructor
 @Entity
+@Getter
 public class  Article {
     @Id //엔티티의 대푯값 지정
     @GeneratedValue //자동생성 기능 추가
@@ -21,9 +24,7 @@ public class  Article {
     private String content;
 
 
-    public Article() {
 
-    }
 }
 
 
--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -3,5 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository <Article, Long>{
+
+    @Override
+    ArrayList<Article> findAll();
 }
